fe434c88e0a77078dfac5ecedd9198f7d5aabd88
GL-ROM <gabriel.l.romero@gmail.com>
2023-02-26 21:50:55 -0500

    commit

--- CS465_Full_Stack_Development_23EW3/travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
+++ CS465_Full_Stack_Development_23EW3/travlr/app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-// import { trips } from '../data/trips';
 import { TripDataService } from '../services/trip-data.service';
 import { Trip } from '../models/trip';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-trip-listing',
@@ -19,7 +19,9 @@ export class TripListingComponent implements OnInit {
 
   constructor(
     private tripDataService: TripDataService,
-    private router: Router) { }
+    private router: Router,
+    private authenticationService: AuthenticationService
+    ) { }
 
   private addTrip(): void {
     this.router.navigate(['add-trip']);
@@ -33,6 +35,10 @@ export class TripListingComponent implements OnInit {
           this.message = foundTrips.length > 0 ? '' : 'No Trips Found';
           this.trips = foundTrips;
         });
+  }
+
+  public isLoggedIn(): boolean {
+    return this.authenticationService.isLoggedIn();
   }
 
   ngOnInit() {
